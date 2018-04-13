c6b5c00965dcc4dc399235e82b4e19eba297008d
BrightFuture <jingqi8@users.noreply.github.com>
2018-04-12 21:07:05 -0700

    Week 8 - Home Work - Support Files

---
+++ helperfunctions.py
@@ -0,0 +1,17 @@
+
+def add(x1, x2):
+    return x1+x2
+
+def diff(x1, x2):
+    return abs(x1-x2)
+
+def product(x1, x2):
+    return x1*x2
+
+def greatest(x1, x2):
+    return max(x1, x2)
+
+if __name__ == "__main__":
+    print "Test....Test...."
+
+### END CODE
---
+++ sorthelper.py
@@ -0,0 +1,9 @@
+
+def sortNumbers(l):
+    return sorted(l, reverse=True)
+
+if __name__ == "__main__":
+    print sortNumbers([3, 5, 7, 2, 9, 1])
+
+
+### END CODE
